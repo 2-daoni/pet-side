2186d624f579cb7da701e0a86309212773f1ae09
이다원 <lyumilove@gmail.com>
2023-06-30 16:06:50 +0900

    [Feat] Theme.ts 생성

---
+++ src/types/Theme.ts
@@ -0,0 +1,13 @@
+import {DefaultTheme} from 'styled-components';
+
+export const Theme: DefaultTheme = {
+  colors: {
+    black: '#000000',
+    white: '#FFFFFF',
+    orangeLight: '#f5eee9',
+    orange: '#e06710',
+    grayLight: '#f7f7f7',
+    gray: '#CACACA',
+    grayDark: '#717171',
+  },
+};

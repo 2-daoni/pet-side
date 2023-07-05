ec90621a924310b05e04348dd743012e255bfcaf
이다원 <lyumilove9@gmail.com>
2023-07-05 19:01:50 +0900

    [Feat] ios splash screen 적용중

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Route from './src/navigations/Route';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <Route />
